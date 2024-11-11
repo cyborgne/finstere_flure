4d201db47db8962413fc3a64a94bc3af4dae1b4e
Cyborgne <tomassoni.nathan@gmail.com>
2024-11-11 19:01:03 +0100

    Ajout de la class Plateau

---
+++ src/Plateau.java
@@ -0,0 +1,25 @@
+import java.util.List;
+
+public class Plateau {
+    private Tuile[][] grilleDeJeu;
+    private List<Pion> listeDePions;
+    private List<ElementDecor> listeElementDeco;
+
+    public void genererSortieJoueur() {
+        // Générer le point de sortie du joueur
+    }
+
+    public void genererEntreeFantome() {
+        // Générer le point d'entrée du fantôme
+    }
+
+    public void genererEntreJoueur() {
+        // Générer le point d'entrée du joueur
+    }
+
+    public void melangerTuile() {
+        // Mélanger les tuiles ou déplacer le monstre
+    }
+
+    // Getters et Setters
+}
