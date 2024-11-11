5c5b9eb113c63ebc6fc6626348e9fe6de74fb64e
Cyborgne <tomassoni.nathan@gmail.com>
2024-11-11 18:58:16 +0100

    Ajout de la class Tuile

---
+++ src/Tuile.java
@@ -0,0 +1,11 @@
+public class Tuile {
+    public Tuile piocher() {
+        // Logique pour piocher une tuile
+        return this;
+    }
+
+    public boolean estUneProie() {
+        // Détermine si c'est une tuile cible
+        return false;
+    }
+}
